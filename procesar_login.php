1f2ad84b23c10ca6672ca8bdea9b3b1e6eac2bbc
Miguel Nazareth <miguelnazareth22@gmail.com>
2023-05-29 18:06:29 -0600

    Mejora autenticacion
    En este cambio se realizo la mejora de la autenticacion en el login

--- procesar_login.php
+++ procesar_login.php
@@ -10,7 +10,7 @@ $nombreUsuario = $_POST['username'];
 $contrasenaUsuario = $_POST['password'];
 
 // Crear conexión a la base de datos
-$conexion = new mysqli($host, $usuario, $contrasena, $baseDatos,3306);
+$conexion = new mysqli($host, $usuario, $contrasena, $baseDatos, 3306);
 
 // Verificar si hay error en la conexión
 if ($conexion->connect_error) {
@@ -20,9 +20,12 @@ if ($conexion->connect_error) {
 // Iniciar sesión
 session_start();
 
-// Consulta SQL para verificar el usuario y la contraseña
-$sql = "SELECT * FROM usuarios WHERE nombreUsuario = '$nombreUsuario' AND contrasena = '$contrasenaUsuario'";
-$resultado = $conexion->query($sql);
+// Consulta SQL para verificar el usuario y la contraseña utilizando consultas preparadas
+$sql = "SELECT * FROM usuarios WHERE nombreUsuario = ? AND contrasena = ?";
+$consultaPreparada = $conexion->prepare($sql);
+$consultaPreparada->bind_param("ss", $nombreUsuario, $contrasenaUsuario);
+$consultaPreparada->execute();
+$resultado = $consultaPreparada->get_result();
 
 // Verificar si se encontró un resultado
 if ($resultado->num_rows > 0) {
@@ -35,6 +38,7 @@ if ($resultado->num_rows > 0) {
     echo 'Usuario o contraseña incorrectos';
 }
 
-// Cerrar la conexión a la base de datos
+// Cerrar la consulta preparada y la conexión a la base de datos
+$consultaPreparada->close();
 $conexion->close();
 ?>
